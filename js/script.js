0a10281e85455caa8d0014ff055a7f2c2f64f6d7
Evgeny Sklyarov <sklyarov.exceedteam@gmail.com>
2019-01-22 17:37:53 +0400

    added a random background color function and check user color function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,25 @@ $(document).ready(function () {
     var btn = $('.todo-btn');
     var todoList = $('.todo-list');
 
+    var userCheckColor = function(colorList) {
+        var color = '';
+
+        $.each(colorList, function (i, item) {
+            if (item.checked) {
+                item.checked = false;
+                return color = item.value;
+            }
+        });
+        
+        return color;
+    };
+
+    var randomCheckColor = function (colorList) {
+        var randomIndex = Math.round(Math.random() * colorList.length);
+
+        return colorList[randomIndex].value;
+    };
+
     btn.on('click', function (event) {
         event.preventDefault();
 
@@ -10,22 +29,21 @@ $(document).ready(function () {
 
         if (toAdd.trim() !== '') {
             toAdd = toAdd.replace(/</g, "&lt;");
-            var bgColor = $('.color-item:checked').val();
             var containerElement = '<div class="checkbox-container"><input class="todo-checkbox" type="checkbox"></div>';
             var spanElement = '<span class="todo-text">' + toAdd +'</span>';
+            var colorList = $('.color-item');
+            var bgColor = userCheckColor(colorList) || randomCheckColor(colorList);    
 
             todoList.append('<li class="todo-task">' + containerElement + spanElement + '</li>');
             $('.todo-task:last-child .checkbox-container').css('background-color', bgColor);
             $('.todo-task:last-child .todo-text').css('background-color', bgColor);
-
-            $('.todo-input').val('');
+            $('.todo-input').val('');     
         } else {
             $('.todo-input').val('');
         }
     });
 
     $(document).on('click','.todo-checkbox', function(e) {
-
         if (e.target.checked === true) {
             $(this).parent().next('.todo-text').addClass('done');
         } else {
